60e47ff3f787224b1436b62d1641f2ab40308fdc
Kitsunechin <martyna.b@hotmail.com>
2020-10-19 18:48:54 +0200

    style fix

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ const Header = () =>
     (
         <header className="App-header">
             <h1>Fitter</h1>
+            <br />
             <div className='subtitle'>With Fitter you can keep track of your daily exercises
            <span> Login or Register and start planning your next fit day!</span>
            </div>
